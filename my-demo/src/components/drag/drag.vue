f3eeaf482468192d2fc6762b0d06e30aad021bcc
EnthusiasmGlitter <lvjiaxuan_098@163.com>
2018-09-06 15:57:59 +0800

    update

--- my-demo/src/components/drag/drag.vue
+++ my-demo/src/components/drag/drag.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="drag">
-   <div class="item"></div>
+   <div class="item">msg</div>
   </div>
 </template>
 
@@ -9,15 +9,13 @@ import { Component, Prop, Watch, Vue, Emit } from "vue-property-decorator";
 
 @Component
 export default class Drag extends Vue {
-  
-  
+  msg: string = "hello";
 }
-
 </script>
 
 <style lang="scss" scoped>
 .drag {
-  .item{
+  .item {
     width: 10px;
     height: 10px;
     border: 1px solid #000;
